0557302c580ba283b6f3c729158fe443662ba605
KiaK <kiak01@students.cs.ubc.ca>
2025-07-15 00:33:09 -0700

    sharpe ratio implementation

--- app/portfolio.py
+++ app/portfolio.py
@@ -2,7 +2,7 @@ from alpaca_api import AlpacaTradingClient
 from dotenv import load_dotenv
 from dataclasses import dataclass
 import os
-
+import sigfig
 
 @dataclass
 class Position:
@@ -10,6 +10,7 @@ class Position:
     quantity: float
     avg_entry_price: float
     current_price: float
+    market_value: float
 
 
 class PortfolioManager:
@@ -17,7 +18,10 @@ class PortfolioManager:
     def __init__(self, api_key, api_secret):
         self.client = AlpacaTradingClient(api_key=api_key, api_secret=api_secret)
 
-    def import_portfolio_from_alpaca(self):
+    def account_details(self):
+        return self.client.get_account()
+
+    def portfolio_positions(self):
         """
         Imports the current portfolio positions using the Alpaca API. Each position is converted into a Position object with relevant attributes
         (symbol, quantity, average entry price, and current price) and returned as a list.
@@ -37,14 +41,31 @@ class PortfolioManager:
                 quantity=float(pos.qty),
                 avg_entry_price=float(pos.avg_entry_price),
                 current_price=float(pos.current_price),
+                market_value=float(pos.market_value),
             )
             for pos in positions
         ]
         return filtered_positions
+
+    def weights_of_positions(self):
+        account = self.account_details()
+        equity = float(account.equity)
+        free_cash = float(account.cash)
+        invested_equity = equity - free_cash
+        if invested_equity == 0:
+            return {}
+        
+        positions = self.portfolio_positions()
+        positions_weights = {
+            pos.symbol: sigfig.round(pos.market_value / invested_equity, sigfigs=3) for pos in positions
+        }
+
+        return positions_weights
+
 
 if __name__ == "__main__":
     load_dotenv()
     api_key = os.getenv("APCA_KEY")
     api_secret = os.getenv("APCA_SECRET")
     manager = PortfolioManager(api_key, api_secret)
-    print(manager.import_portfolio_from_alpaca())
+    print(manager.portfolio_positions())
